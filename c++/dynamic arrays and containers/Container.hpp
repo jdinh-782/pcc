55c863fb5a0faf1e180f2428ee816dd0bc4f20b3
jdinh <52547731+jdinh-782@users.noreply.github.com>
2021-04-25 18:13:32 -0700

    Add files via upload

---
+++ c++/dynamic arrays and containers/Container.hpp
@@ -0,0 +1,54 @@
+//
+// Name: Johnson Dinh
+// SSID: 1857773
+// Assignment #: 2
+// Submission Date: 3/23/21
+//
+
+
+#ifndef _CONTAINER_HPP
+#define _CONTAINER_HPP
+
+#include <iostream>
+
+using namespace std;
+
+
+template <class T>
+class Container
+{
+public:
+    //destructor
+    virtual ~Container();
+
+    //create the dynamic array with size s
+    virtual bool resize (int s) = 0;
+
+    //checks if the container has no elements
+    virtual bool empty() = 0;
+
+    //returns the number of elements in and/or size of the container for an array size and memsize will be the same
+    virtual int size() = 0;
+
+    virtual T* begin() = 0;
+    virtual T* end() = 0;
+
+    //for a container c, the expression c.front() is equivalent to *c.begin(); undefined if empty()
+    virtual T& front() = 0;
+
+    //if size() is 0, data() may or may not return a null pointer
+    virtual T* data() = 0;
+    virtual const T* data() const = 0;
+
+    //return reference to the requested element
+    virtual T& operator [] (int position) = 0;
+
+};
+
+
+template<class T>
+Container<T>::~Container()
+= default;
+
+
+#endif
---
+++ c++/dynamic arrays and containers/DynArray.hpp
@@ -0,0 +1,189 @@
+//
+// Name: Johnson Dinh
+// SSID: 1857773
+// Assignment #: 2
+// Submission Date: 3/23/21
+//
+
+#ifndef _DYNARRAY_HPP
+#define _DYNARRAY_HPP
+
+#include "Container.hpp"
+
+
+template <class T>
+class DynArray : public Container<T>
+{
+public:
+    DynArray<T>() : memsize(0), _data(nullptr)
+    {
+
+    }
+
+    DynArray<T>(int size, initializer_list<T> il);
+
+    DynArray<T>(const DynArray<T>& arr);
+
+    virtual ~DynArray<T>();
+
+    //TODO A2: override all container virtual functions
+    bool resize(int s);
+    bool empty();
+    int size();
+    T* begin();
+    T* end();
+    T& front();
+    T& back();
+    T& at(int position);
+    T* data();
+    const T* data() const;
+    T& operator [] (int position);
+
+
+private:
+    int memsize = 0;
+    T* _data = nullptr;
+
+};
+
+
+template<class T>
+DynArray<T>::DynArray(int size, initializer_list<T> il)
+{
+    _data = new T[size];
+    for (auto item : il)
+    {
+        _data[memsize] = item;
+        memsize++;
+    }
+}
+
+
+template<class T>
+DynArray<T>::DynArray(const DynArray<T>& arr)
+{
+    _data = new T[arr.memsize];
+    for (auto item : arr)
+    {
+        _data[memsize] = item;
+        memsize++;
+    }
+}
+
+
+template<class T>
+DynArray<T>::~DynArray()
+{
+    delete [] _data;
+}
+
+
+template<class T>
+bool DynArray<T>::resize(int s)
+{
+    memsize = s;
+    _data = new T[memsize];
+
+    //check if there is no first data
+    if (_data == nullptr)
+    {
+        return false;
+    }
+    return true;
+}
+
+
+template<class T>
+bool DynArray<T>::empty()
+{
+    if (memsize == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+
+template<class T>
+int DynArray<T>::size()
+{
+    return memsize;
+}
+
+
+template<class T>
+T* DynArray<T>::begin()
+{
+    if (empty())
+    {
+        return nullptr;
+    }
+    return &_data[0];
+}
+
+
+template<class T>
+T* DynArray<T>::end()
+{
+    return begin() + memsize;
+}
+
+
+template<class T>
+T& DynArray<T>::front()
+{
+    return *begin();
+}
+
+
+template<class T>
+T& DynArray<T>::back()
+{
+    return *prev(end());
+}
+
+
+template<class T>
+T& DynArray<T>::at(int position)
+{
+    try
+    {
+        if (position >= memsize || position < 0)
+        {
+            throw "\nIndex is out of range!\n";
+        }
+        return _data[position];
+    }
+    catch (const char* e)
+    {
+        cout << e;
+    }
+}
+
+
+template<class T>
+T* DynArray<T>::data()
+{
+    if (size() == 0)
+    {
+        return nullptr;
+    }
+    return _data;
+}
+
+
+template<class T>
+const T* DynArray<T>::data() const
+{
+    return _data;
+}
+
+
+template<class T>
+T& DynArray<T>::operator [] (int position)
+{
+    return _data[position];
+}
+
+
+#endif
