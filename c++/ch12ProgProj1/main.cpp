877d5c276d57deea0da44f65f030a50f39b5df8d
jdinh <52547731+jdinh-782@users.noreply.github.com>
2021-04-25 17:10:28 -0700

    Create main.cpp

---
+++ c++/ch12ProgProj1/main.cpp
@@ -0,0 +1,20 @@
+//Chapter 12 Programming Project 1
+
+
+#include "interface.h"
+
+int main()
+{
+    interface i;
+
+    i.getElements();
+    i.displaySet();
+
+    i.createUnion();
+    i.displayUnion();
+
+    i.createIntersection();
+    i.displayIntersection();
+
+    return 0;
+}
